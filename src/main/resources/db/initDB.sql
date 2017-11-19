c3bd07cfac17b44c70f4693886ee6a6bb429c9f7
MeldenevAV <sashavladimirovich3@gmail.com>
2017-11-19 10:56:34 +0300

    make 1, 2

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS meals;
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START 100000;
@@ -14,12 +15,22 @@ CREATE TABLE users
   enabled          BOOL DEFAULT TRUE       NOT NULL,
   calories_per_day INTEGER DEFAULT 2000    NOT NULL
 );
-CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
+CREATE UNIQUE INDEX users_unique_email_idx
+  ON users (email);
 
 CREATE TABLE user_roles
 (
   user_id INTEGER NOT NULL,
   role    VARCHAR,
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE meals
+(
+  user_id     INTEGER                 NOT NULL,
+  description VARCHAR                 NOT NULL,
+  datetime    TIMESTAMP DEFAULT now() NOT NULL,
+  calories    INTEGER                 NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -9,3 +10,13 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id, description, datetime, calories) VALUES
+  (100000, 'Завтрак', TIMESTAMP '2015-05-30 10:00', 500),
+  (100000, 'Обед', TIMESTAMP '2015-05-30 13:00', 1000),
+  (100000, 'Ужин', TIMESTAMP '2015-05-30 20:00', 500),
+  (100000, 'Завтрак', TIMESTAMP '2015-05-31 10:00', 1000),
+  (100000, 'Обед', TIMESTAMP '2015-05-31 13:00', 500),
+  (100000, 'Ужин', TIMESTAMP '2015-05-31 20:00', 510),
+  (100001, 'Админ ланч', TIMESTAMP '2015-06-01 14:00', 510),
+  (100001, 'Админ ужин', TIMESTAMP '2015-06-01 21:00', 1500);
